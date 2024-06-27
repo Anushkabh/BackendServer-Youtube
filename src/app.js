33e68f62eec9cebe6dbff8c75fca68cd7be0aa2b
anushka bhandari <bhandarianushka670@gmail.com>
2024-06-27 16:31:33 +0530

    chore: Update package.json with "type" field and add nodemon script

---
+++ src/app.js
@@ -0,0 +1,28 @@
+import express from "express";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import morgan from "morgan";
+
+const app = new express();
+
+app.use(
+    cors({
+        origin: process.env.CORS_ORIGIN,
+        credentials: true,
+    })
+);
+
+app.use(express.json({ limit: "50mb" }));
+app.use(express.urlencoded({ extended: true, limit: "50mb" }));
+app.use(express.static("public"));
+app.use(cookieParser());
+app.use(morgan("dev")); //HTTP request logger middleware for node.js 
+
+
+
+
+
+
+
+
+export default app;
